c618cd6c61ce508c4765d52353a0ff4bc3056fa9
Mircea Tanasa <tanasamircea@yahoo.com>
2015-09-03 14:11:44 +0300

    *Fixed the project filter

--- data/products.php
+++ data/products.php
@@ -4,34 +4,33 @@ $password = "root";
 $hostname = "localhost"; 
 
 
-//connection to the database
 $dbhandle = mysql_connect($hostname, $username, $password) 
  or die("Unable to connect to MySQL");
 
-//select a database to work with
 $selected = mysql_select_db("Shop",$dbhandle) 
   or die("Could not select shop");
 
 $limit = $_GET["limit"];
-//execute the SQL query and return records
-$result = mysql_query(" SELECT name, price, picture FROM Products LIMIT $limit ");
 
+$result = mysql_query(" SELECT Id, name, price, picture FROM Products LIMIT $limit ");
+    
 
-//fetch tha data from the database 
-	
 	$jsonData = array();
 	while ($res = mysql_fetch_object($result)) {
-    	
+        $curr = $res->Id;    	
+        $cats = mysql_query("SELECT Catname as category FROM Categories INNER JOIN ProductCategories 
+            ON Categories.ID = ProductCategories.CategoryId INNER JOIN Products ON ProductCategories.ProductId=Products.Id
+            WHERE Products.Id = $curr");
+        $jsonCategories = array();
+        while ($cat=mysql_fetch_assoc($cats)) {
+            $jsonCategories[]=$cat['category']; 
+        }
 
-
-    	
+        $res->categoriesRaw = $jsonCategories;
     	$jsonData[] = $res;
-	
 	}
 	echo json_encode($jsonData, true);
 
-
-//close the connection
 mysql_close($dbhandle);
 
 //SELECT Products.Name, Categories.Name FROM Products INNER JOIN ProductCategories ON Products.Id=ProductCategories.ProductId INNER JOIN Categories ON ProductCategories.CategoryId=Categories.ID WHERE Products.Id=1
