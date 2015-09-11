2d06bb58a4e1d5d9ad5527c959e6f885e5047bed
Mircea Tanasa <tanasamircea@yahoo.com>
2015-09-11 11:22:14 +0300

    *Added a new price range (uglier but mine)

--- app/filter/priceRange/priceRangeDirective.js
+++ app/filter/priceRange/priceRangeDirective.js
@@ -23,71 +23,50 @@ angular
     function priceRangeController(filterRangeService){
 
     	var vm = this;
-
     	vm.minRange = 0;
 		vm.maxRange = 100;
-
 		vm.filterMinRange = vm.minRange;
 		vm.filterMaxRange = vm.maxRange;
-
     	vm.setMinMaxRange = setMinMaxRange;
 
+    	vm.getDomElement = getDomElement;
 
     	function getDomElement(element) {
 			return document.querySelector(element);
 		};
-		function ngElement(element) {
-			return angular.element(element);
-		};
-	    var rngCont    = getDomElement('.rangeContainer'),
-			rangeLeft  = getDomElement('.rangeLeft'),
-			rangeRight = getDomElement('.rangeRight'),
-			spinLeft   = getDomElement('.spinLeft'),
-			spinRight  = getDomElement('.spinRight');
+		var range = vm.getDomElement('.range'),
+ 	        left = vm.getDomElement('.range_left'),
+	        right = vm.getDomElement('.range_right'),
+	        rangeLeft = Math.round( range.getBoundingClientRect().left),
+	        rangeWidth = range.getBoundingClientRect().width,
+ 	        newVal;  
 
-		var box = rngCont.getBoundingClientRect();
-		var sliderBoxCoordsLeft = Math.round(box.left);
-		var containerWidth = rngCont.offsetWidth;
-
-		function positions(spin) {
-			document.onmousemove = function(event) {
-				var positionPercent = ((event.pageX - sliderBoxCoordsLeft)/containerWidth)*100;
-				var newPosRightSpin = 100-positionPercent;
-				var range = Math.round((vm.maxRange/100)*positionPercent);
-
-				if (spin == 'left' && positionPercent <= 100) {
-					range = (range <= 0) ? 0 : range;
-					if (range >= vm.minRange && range < vm.filterMaxRange) {
-						rangeLeft.style.width = positionPercent+'%';
-						vm.filterMinRange = range;
-						vm.setMinMaxRange('Min');
-					}
-				}
-				if (spin == 'right' && newPosRightSpin <= 100) {
-					range = (range >= vm.maxRange) ? vm.maxRange : range;
-						if(range > vm.filterMinRange){
-						rangeRight.style.width = newPosRightSpin+'%';
-						vm.filterMaxRange = range;
-						vm.setMinMaxRange('Max');
-					}
-				}
-			}
-			document.onmouseup = function() {
-				ngElement(spinRight).removeClass('active');
-				ngElement(spinLeft).removeClass('active');
-				document.onmousemove = document.onmouseup = null;
-			}
-		};
-
-		spinLeft.onmousedown = function() {
-			ngElement(spinLeft).addClass('active');
-			positions('left');
-		};
-		spinRight.onmousedown = function() {
-			ngElement(spinRight).addClass('active');
-			positions('right');
-		};
-
+	    left.onmousedown = function(){
+	        startMoving('left');
+	    }
+	    right.onmousedown = function(){
+	        startMoving('right');
+	    }
+	    function startMoving(direction){
+		      document.onmousemove = function(){
+		    	var clickPos = ((event.pageX - rangeLeft)/rangeWidth)*100;
+		        if(direction === 'left' && clickPos <= vm.filterMaxRange){
+		        	left.style.width = clickPos + '%';
+		        	newVal = (clickPos <= vm.minRange) ? vm.minRange : clickPos; 
+		        	vm.filterMinRange = Math.round(newVal);
+		        	vm.setMinMaxRange('Min');
+		        }
+		      	if(direction === 'right' && clickPos >= vm.filterMinRange){
+		        	right.style.width = (100-clickPos) + '%';
+		        	newVal = (clickPos >= vm.maxRange) ? vm.maxRange : clickPos; 
+		        	vm.filterMaxRange = Math.round(newVal);
+		        	vm.setMinMaxRange('Max');
+		      	}
+		      }
+		      document.onmouseup = function(){
+		      	document.onmousemove = null;
+		      }
+		  }
 		function setMinMaxRange(MinOrMax){
             if(MinOrMax == 'Min'){
                 filterRangeService.sendMinRange(vm.filterMinRange);
@@ -96,11 +75,9 @@ angular
                 filterRangeService.sendMaxRange(vm.filterMaxRange);
             }
         };
-
 	   	filterRangeService.minMaxRangeSent().then(null, null, function(data){
 	   		vm.minRange = data.min;
 			vm.maxRange = data.max;
-
         });
     }
 })();
--- data/buyCart.php
+++ data/buyCart.php
@@ -4,12 +4,10 @@ $password = "root";
 $hostname = "localhost"; 
 $database = "Shop";
 
-
 $conn = new mysqli($hostname, $username, $password, $database);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-
 $newOrder = "INSERT INTO OrdersBig (buyer)
 	VALUES ('John')";
 
@@ -22,26 +20,21 @@ if ($conn->query($newOrder) === TRUE) {
     $currOrder = mysqli_insert_id($conn);
 
 	foreach ($data as $value){  
-
 		$id = $value['Id'];
 		$qInCart = $value['qInCart'];
-
-	    $newOrderLine = "INSERT INTO Orders ( id, productId, quantity) VALUES ('$currOrder','$id','$qInCart')";
+	    $newOrderLine = "INSERT INTO Orders ( id, productId, quantity) 
+	    				 VALUES ('$currOrder','$id','$qInCart')";
 	    if ($conn->query($newOrderLine) === TRUE) {
-	    	
 	    }
 	    else{
 	    	echo "Error: " . $newOrderLine . "<br>" . $conn->error;
 	    }
-
 	}
 	echo json_encode($data);
-
 } 
 else {
     echo "Error: " . $newOrder . "<br>" . $conn->error;
 }
-
 $conn->close();
 
 /*
